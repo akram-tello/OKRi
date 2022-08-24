485e9e038ae1a6baee340d5e66fee638458ea470
akram-tello <a.tello@graduate.utm.my>
2022-08-24 08:52:16 +0800

    update main js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: Moderna - v4.9.1
-* Template URL: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
