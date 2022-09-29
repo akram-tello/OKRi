35bf148c19fe2b13d25635e6aee542b2218e134c
akram-tello <a.tello@graduate.utm.my>
2022-09-30 04:28:11 +0800

    update content & added price section

--- assets/js/main.js
+++ assets/js/main.js
@@ -201,4 +201,38 @@
    */
   new PureCounter();
 
+  var swiper = new Swiper(".mySwiper", {
+    loop: false,
+    loopFillGroupWithBlank: false,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: "2",
+        spaceBetween: 16,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: "2",
+        spaceBetween: 16,
+      },
+      // when window width is >= 640px
+      768: {
+        slidesPerView: "3",
+        spaceBetween: 24,
+      },
+      1024: {
+        slidesPerView: "3",
+        spaceBetween: 32,
+      },
+      1336: {
+        slidesPerView: "5",
+        spaceBetween: 32,
+      },
+    },
+  });
+  
 })()
